cf0d4df15d2a8128a32a08cbef9b958c4768004e
sulkantisiri0424-lang <sulkantisiri0424@gmail.com>
2026-07-12 13:14:16 +0530

    Added script.js

--- static/script.js
+++ static/script.js
@@ -1,18 +1,34 @@
+
 function sendMessage() {
-    const message = document.getElementById("message").value;
+    let input = document.getElementById("user-input");
+    let chatBox = document.getElementById("chat-box");
+
+    let message = input.value.trim();
+
+    if (message === "") return;
+
+    chatBox.innerHTML += `<div class="user"><b>You:</b> ${message}</div>`;
 
     fetch("/chat", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ message: message })
+        body: JSON.stringify({
+            message: message
+        })
     })
     .then(response => response.json())
     .then(data => {
-        const chatBox = document.getElementById("chat-box");
-        chatBox.innerHTML += "<p><b>You:</b> " + message + "</p>";
-        chatBox.innerHTML += "<p><b>Bot:</b> " + data.response + "</p>";
-        document.getElementById("message").value = "";
+        chatBox.innerHTML += `<div class="bot"><b>Bot:</b> ${data.response}</div>`;
+        chatBox.scrollTop = chatBox.scrollHeight;
     });
+
+    input.value = "";
 }
+
+document.getElementById("user-input").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        sendMessage();
+    }
+});
